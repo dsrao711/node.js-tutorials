93d17b0eddbe5ab5434ec2177759fcdf50ccd72c
Divya <dsrao0712@gmail.com>
2021-08-07 01:59:01 +0530

    Document object model

--- dom/index.js
+++ dom/index.js
@@ -6,10 +6,39 @@ const div = document.querySelector('body > div > p:nth-child(2)')
 
 //All
 const params = document.querySelectorAll('p')
+const params = document.getElementsByTagName('p')
 
-console.log(params)
-console.log(params[1])
+// console.log(params)
+// console.log(params[1])
 
 params.forEach(param => {
     console.log(param);
 })
+
+// Change inner text
+
+const p = document.querySelector('p')
+p.innerText = ('Hi , divya here!')
+
+const paras = document.querySelectorAll('p')
+
+paras.forEach(para => {
+    console.log(para.innerText)
+    para.innerText += ' inner text changed'
+});
+
+// Change Inner Html
+
+const content = document.querySelector('.content')
+
+// content.innerHTML = '<H2>This is new inner HTML</H2>'
+
+const people = ['Divya', 'Person1', 'Person2'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`
+})
+
+const title = document.querySelector('h2')
+
+title.setAttribute('style', 'color:yellow;')
