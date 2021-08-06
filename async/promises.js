582c29fa5dc1d6ac7e4ec7d567838ecc22f18125
Divya <dsrao0712@gmail.com>
2021-08-06 21:35:50 +0530

    Promise all

--- async/promises.js
+++ async/promises.js
@@ -33,6 +33,21 @@ function createPost(post) {
     });
 }
 
-createPost({ title: 'Post three', body: 'This is post three' })
-    .then(getPosts)
-    .catch(err => console.log(err));
+// createPost({ title: 'Post three', body: 'This is post three' })
+//     .then(getPosts)
+//     .catch(err => console.log(err));
+
+// Promise.all
+const promise1 = Promise.resolve('Hwelle');
+const promise2 = 10
+const promise3 = new Promise((resolve, reject) =>
+    setTimeout(resolve, 2000, 'GooGoo')
+);
+
+const promise4 = fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json());
+
+
+Promise.all([promise1, promise2, promise3, promise4]).then(
+    values => console.log(values)
+);
