9298224c922dc91bf4b6a4e17f7cd3e23bc7748e
Divya <dsrao0712@gmail.com>
2021-08-07 20:20:58 +0530

    Files

--- node.js-basics/files.js
+++ node.js-basics/files.js
@@ -1,42 +1,50 @@
-// Os module
+// // Os module
 
-// const os = require('os');
-// console.log(os.homedir());
+// // const os = require('os');
+// // console.log(os.homedir());
 
 const fs = require('fs')
 
-// reading files
-// fs.readFile('./docs/test.txt', (err, data) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(data.toString())
+// // reading files
+// // fs.readFile('./docs/test.txt', (err, data) => {
+// //     if (err) {
+// //         console.log(err);
+// //     }
+// //     console.log(data.toString())
+// // });
+
+// // writing files
+// fs.writeFile('./docs/test.txt', 'Hello world!', () => {
+//     console.log('Writen')
 // });
 
-// writing files
-fs.writeFile('./docs/test.txt', 'Hello world!', () => {
-    console.log('Writen')
-});
+// fs.writeFile('./docs/test_2.txt', 'Hello world!', () => {
+//     console.log('Writen')
+// });
+// // directories
 
-fs.writeFile('./docs/test_2.txt', 'Hello world!', () => {
-    console.log('Writen')
-});
-// directories
-
-if (!fs.existsSync('./assets')) {
-    fs.mkdir('./assets', (err) => {
-        if (err) {
-            console.log(err)
-        }
-        console.log('Folder created')
-    });
-} else {
-    fs.rmdir('./assets', (err) => {
-        if (err) {
-            console.log('Error')
-        }
-        console.log('Folder deleted')
-    })
-}
+// if (!fs.existsSync('./assets')) {
+//     fs.mkdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         console.log('Folder created')
+//     });
+// } else {
+//     fs.rmdir('./assets', (err) => {
+//         if (err) {
+//             console.log('Error')
+//         }
+//         console.log('Folder deleted')
+//     })
+// }
 
 // deleting files
+if (fs.existsSync('./docs/delete.txt')) {
+    fs.unlink('./docs/delete.txt', err => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('file deleted');
+    });
+}
