0cfa0bcfb0cda199996810bc484df4d93842142e
Divya <dsrao0712@gmail.com>
2021-08-08 21:03:44 +0530

    Status codes and redirects

--- app/server.js
+++ app/server.js
@@ -1,9 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 
-
-
 const server = http.createServer((req, res) => {
+
     console.log('Request has been made')
     res.setHeader('Content-Type', 'text/html');
     let path = './views/'
@@ -11,20 +10,30 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
             path += 'index.html'
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            path += 'about.html'
+            res.statusCode = 200;
             break;
         case '/about':
-            path += 'about.html'
+            res.statusCode = 301;
+            res.setHeader('Location', '/about-me');
+            res.end();
             break;
         default:
             path += '404.html'
-            break;
+            res.statusCode = 404;
+
     }
 
     fs.readFile(path, (err, data) => {
+
         if (err) {
             console.log(err);
             res.end();
         } else {
+
             res.end(data)
         }
     })
