d40aa7dd348034ac3c3d23abe7fd8670e341aa49
Divya <dsrao0712@gmail.com>
2021-08-11 23:26:45 +0530

    ejs

--- app/app.js
+++ app/app.js
@@ -5,8 +5,13 @@ const app = express();
 // Listen for request
 app.listen(3000);
 
+// Set View engine
+
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index')
 });
 
 app.get('/about', (req, res) => {
@@ -18,4 +23,9 @@ app.get('/about', (req, res) => {
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
 });
+
 // 404 pages
+
+app.use((req, res) => {
+    res.status(400).sendFile('./views/404.html', { root: __dirname })
+});
