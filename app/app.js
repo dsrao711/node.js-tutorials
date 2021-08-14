fd69dc8ffbf251581233b0d1e877c444697574bb
Divya <dsrao0712@gmail.com>
2021-08-14 23:30:04 +0530

    mongoose

--- app/app.js
+++ app/app.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 // morgan - HTTP request logger middleware for node.js
 const morgan = require('morgan')
+const mongoose = require('mongoose');
+
+// Connect to Mongo DB
+
+const dburl = 'mongodb+srv://divya:test1234@cluster0.nk5k9.mongodb.net/Blog?retryWrites=true&w=majority'
+
+mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // Listen for request
-app.listen(3000);
+
 
 // Set View engine
 app.set('view engine', 'ejs')
@@ -15,7 +24,6 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-
     res.render('index', { title: "Brooklyn 999" })
 });
 
